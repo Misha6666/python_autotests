8cd7fc339f2efcb1e0cc27ba7545b7dd4211791e
Misha6666 <75987160+Misha6666@users.noreply.github.com>
2025-01-08 18:58:23 +0400

    Update test_pokemon.py

--- PythonProjects/tests/test_pokemon.py
+++ PythonProjects/tests/test_pokemon.py
@@ -13,11 +13,3 @@ def test_me_trainer():
     trainer = json.loads(trainer.text)
     assert trainer["data"][0]["trainer_name"] == 'DIO'
 
-
-def test_trainers():
-    trainers = requests.get(url=f"{URL}/trainers",headers=Header)
-    assert trainers.status_code == 200
-
-def test_me_trainer():
-    trainers = requests.get(url=f"{URL}/trainers?trainer_id=11644",headers=Header)
-    assert trainers.status_code == 200
